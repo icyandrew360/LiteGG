a884c0d937786a07e6fed22b4bf1b28bd4feb74a
Andrew Howe <andrewhowe1000@gmail.com>
2024-12-28 15:18:01 -0700

    change port

--- app/src/stat-screens/UrlConsts.tsx
+++ app/src/stat-screens/UrlConsts.tsx
@@ -1,4 +1,4 @@
 export const RENDER_BASE_URL='https://litegg.onrender.com';
 export const LOCAL_BASE_URL='http://localhost:10000';
 
-export const CURRENT_BASE_URL=RENDER_BASE_URL;
+export const CURRENT_BASE_URL=LOCAL_BASE_URL;
--- main.py
+++ main.py
@@ -80,4 +80,4 @@ async def api_key_test():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
